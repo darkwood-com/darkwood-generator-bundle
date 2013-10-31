a096c11f8e848617a1332d7978b6e50c0fa34c18
Mathieu Ledru <matyo91@gmail.com>
2013-10-31 18:13:05 +0100

    to-bundle

--- Command/GenerateDoctrineCrudCommand.php
+++ Command/GenerateDoctrineCrudCommand.php
@@ -14,6 +14,10 @@ namespace Darkwood\GeneratorBundle\Command;
 use Sensio\Bundle\GeneratorBundle\Command\GenerateDoctrineCrudCommand as BaseCommand;
 use Darkwood\GeneratorBundle\Generator\DoctrineCrudGenerator;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Sensio\Bundle\GeneratorBundle\Command\Validators;
 
 /**
  * Generates a CRUD for a Doctrine entity.
@@ -27,9 +31,137 @@ class GenerateDoctrineCrudCommand extends BaseCommand
         parent::configure();
 
         $this->setName('darkwood:generate:doctrine:crud');
+        $this
+            ->getDefinition()
+            ->addOption(new InputOption('to-bundle', '', InputOption::VALUE_REQUIRED, 'The bundle where to put crud generation'));
     }
 
-    protected function createGenerator()
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getDialogHelper();
+
+        if ($input->isInteractive()) {
+            if (!$dialog->askConfirmation($output, $dialog->getQuestion('Do you confirm generation', 'yes', '?'), true)) {
+                $output->writeln('<error>Command aborted</error>');
+
+                return 1;
+            }
+        }
+
+        $entity = Validators::validateEntityName($input->getOption('entity'));
+        list($bundle, $entity) = $this->parseShortcutNotation($entity);
+
+        $toBundle = Validators::validateBundleName($input->getOption('to-bundle'));
+        $format = Validators::validateFormat($input->getOption('format'));
+        $prefix = $this->getRoutePrefix($input, $entity);
+        $withWrite = $input->getOption('with-write');
+        $forceOverwrite = $input->getOption('overwrite');
+
+        $dialog->writeSection($output, 'CRUD generation');
+
+        $entityClass = $this->getContainer()->get('doctrine')->getAliasNamespace($bundle).'\\'.$entity;
+        $metadata    = $this->getEntityMetadata($entityClass);
+        $bundle      = $this->getContainer()->get('kernel')->getBundle($bundle);
+        $toBundle    = $this->getContainer()->get('kernel')->getBundle($toBundle);
+
+        $generator = $this->getGenerator($bundle);
+        $generator->generateToBundle($bundle, $entity, $toBundle, $metadata[0], $format, $prefix, $withWrite, $forceOverwrite);
+
+        $output->writeln('Generating the CRUD code: <info>OK</info>');
+
+        $errors = array();
+        $runner = $dialog->getRunner($output, $errors);
+
+        // form
+        if ($withWrite) {
+            $this->generateForm($bundle, $entity, $metadata);
+            $output->writeln('Generating the Form code: <info>OK</info>');
+        }
+
+        // routing
+        if ('annotation' != $format) {
+            $runner($this->updateRouting($dialog, $input, $output, $bundle, $format, $entity, $prefix));
+        }
+
+        $dialog->writeGeneratorSummary($output, $errors);
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getDialogHelper();
+        $dialog->writeSection($output, 'Welcome to the Doctrine2 CRUD generator');
+
+        // namespace
+        $output->writeln(array(
+            '',
+            'This command helps you generate CRUD controllers and templates.',
+            '',
+            'First, you need to give the entity for which you want to generate a CRUD.',
+            'You can give an entity that does not exist yet and the wizard will help',
+            'you defining it.',
+            '',
+            'You must use the shortcut notation like <comment>AcmeBlogBundle:Post</comment>.',
+            '',
+        ));
+
+        $entity = $dialog->askAndValidate($output, $dialog->getQuestion('The Entity shortcut name', $input->getOption('entity')), array('Sensio\Bundle\GeneratorBundle\Command\Validators', 'validateEntityName'), false, $input->getOption('entity'));
+        $input->setOption('entity', $entity);
+        list($bundle, $entity) = $this->parseShortcutNotation($entity);
+
+        // to-bundle
+        $toBundle = $bundle;
+        $output->writeln(array(
+            '',
+            'Determine the bundle where to put the generated CRUD.',
+            '',
+        ));
+        $toBundle = $dialog->askAndValidate($output, $dialog->getQuestion('Which bundle you want to put the crud generation', $toBundle), array('Sensio\Bundle\GeneratorBundle\Command\Validators', 'validateBundleName'), false, $toBundle);
+        $input->setOption('to-bundle', $toBundle);
+
+        // write?
+        $withWrite = $input->getOption('with-write') ?: false;
+        $output->writeln(array(
+            '',
+            'By default, the generator creates two actions: list and show.',
+            'You can also ask it to generate "write" actions: new, update, and delete.',
+            '',
+        ));
+        $withWrite = $dialog->askConfirmation($output, $dialog->getQuestion('Do you want to generate the "write" actions', $withWrite ? 'yes' : 'no', '?'), $withWrite);
+        $input->setOption('with-write', $withWrite);
+
+        // format
+        $format = $input->getOption('format');
+        $output->writeln(array(
+            '',
+            'Determine the format to use for the generated CRUD.',
+            '',
+        ));
+        $format = $dialog->askAndValidate($output, $dialog->getQuestion('Configuration format (yml, xml, php, or annotation)', $format), array('Sensio\Bundle\GeneratorBundle\Command\Validators', 'validateFormat'), false, $format);
+        $input->setOption('format', $format);
+
+        // route prefix
+        $prefix = $this->getRoutePrefix($input, $entity);
+        $output->writeln(array(
+            '',
+            'Determine the routes prefix (all the routes will be "mounted" under this',
+            'prefix: /prefix/, /prefix/new, ...).',
+            '',
+        ));
+        $prefix = $dialog->ask($output, $dialog->getQuestion('Routes prefix', '/'.$prefix), '/'.$prefix);
+        $input->setOption('route-prefix', $prefix);
+
+        // summary
+        $output->writeln(array(
+            '',
+            $this->getHelper('formatter')->formatBlock('Summary before generation', 'bg=blue;fg=white', true),
+            '',
+            sprintf("You are going to generate a CRUD controller for \"<info>%s:%s</info>\"", $bundle, $entity),
+            sprintf("using the \"<info>%s</info>\" format.", $format),
+            '',
+        ));
+    }
+
+    protected function createGenerator($bundle = null)
     {
         return new DoctrineCrudGenerator($this->getContainer()->get('filesystem'));
     }
--- Generator/DoctrineCrudGenerator.php
+++ Generator/DoctrineCrudGenerator.php
@@ -12,6 +12,8 @@
 namespace Darkwood\GeneratorBundle\Generator;
 
 use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator as BaseGenerator;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
 
 /**
  * Generates a CRUD controller.
@@ -20,5 +22,83 @@ use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator as BaseGenerat
  */
 class DoctrineCrudGenerator extends BaseGenerator
 {
+    protected $toBundle;
 
+    /**
+     * Generate the CRUD controller.
+     *
+     * @param BundleInterface   $bundle           A bundle object
+     * @param string            $entity           The entity relative class name
+     * @param ClassMetadataInfo $metadata         The entity class metadata
+     * @param string            $format           The configuration format (xml, yaml, annotation)
+     * @param string            $routePrefix      The route name prefix
+     * @param array             $needWriteActions Wether or not to generate write actions
+     *
+     * @throws \RuntimeException
+     */
+    public function generateToBundle(BundleInterface $bundle, $entity, BundleInterface $toBundle, ClassMetadataInfo $metadata, $format, $routePrefix, $needWriteActions, $forceOverwrite)
+    {
+        $this->routePrefix = $routePrefix;
+        $this->routeNamePrefix = str_replace('/', '_', $routePrefix);
+        $this->actions = $needWriteActions ? array('index', 'show', 'new', 'edit', 'delete') : array('index', 'show');
+
+        if (count($metadata->identifier) > 1) {
+            throw new \RuntimeException('The CRUD generator does not support entity classes with multiple primary keys.');
+        }
+
+        if (!in_array('id', $metadata->identifier)) {
+            throw new \RuntimeException('The CRUD generator expects the entity object has a primary key field named "id" with a getId() method.');
+        }
+
+        $this->entity   = $entity;
+        $this->bundle   = $bundle;
+        $this->toBundle = $toBundle;
+        $this->metadata = $metadata;
+        $this->setFormat($format);
+
+        $this->generateControllerClass($forceOverwrite);
+
+        $dir = sprintf('%s/Resources/views/%s', $this->toBundle->getPath(), str_replace('\\', '/', $this->entity));
+
+        if (!file_exists($dir)) {
+            $this->filesystem->mkdir($dir, 0777);
+        }
+
+        $this->generateIndexView($dir);
+
+        if (in_array('show', $this->actions)) {
+            $this->generateShowView($dir);
+        }
+
+        if (in_array('new', $this->actions)) {
+            $this->generateNewView($dir);
+        }
+
+        if (in_array('edit', $this->actions)) {
+            $this->generateEditView($dir);
+        }
+
+        $this->generateTestClass();
+        $this->generateConfiguration();
+    }
+
+    /**
+     * Sets the configuration format.
+     *
+     * @param string $format The configuration format
+     */
+    private function setFormat($format)
+    {
+        switch ($format) {
+            case 'yml':
+            case 'xml':
+            case 'php':
+            case 'annotation':
+                $this->format = $format;
+                break;
+            default:
+                $this->format = 'yml';
+                break;
+        }
+    }
 }
